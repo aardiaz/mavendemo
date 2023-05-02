6b63faf737e5ab194bef9b8d2eb34eb2f0ae1126
arjun <arjunrokaya@gmail.com>
2023-05-02 15:04:12 +0545

    maven framework

---
+++ src/main/java/bway/mavendemo2pm/App.java
@@ -0,0 +1,13 @@
+package bway.mavendemo2pm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
